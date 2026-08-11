5054333ddeed9a8f777c404a4a12df37e3cbaaeb
Nitin-Singh922 <nitinsingh95555@gmail.com>
2026-08-11 19:03:06 +0000

    Added Variables file

---
+++ Basics/variables.js
@@ -0,0 +1,12 @@
+const accId = 5577
+let accEmail = "nitinsingh9555@gmail.com"
+var accPass = "1234"
+accCity = "Gorakhpur"
+
+//accId = 5757
+accEmail = "singhnitin5521"
+accPass = 34
+accCity = "Payagraj"
+
+console.log(accId);
+console.log(accId, accEmail, accPass, accCity);
